5a8343878c625984534b1c866b2f0e07611aee0e
Scuit <100394853+Scuity@users.noreply.github.com>
2022-05-30 06:40:50 +0900

    初版无响应式

---
+++ work.js
@@ -0,0 +1,19 @@
+function liandong(){
+    window.location.href="new-liandong.html";
+}
+
+function jianmo(){
+    window.location.href="new-3d.html";
+}
+
+function stickers(){
+    window.location.href="new-stickers.html";
+}
+
+function live61(){
+    window.location.href="new-61live.html";
+}
+
+function backtonews(){
+    window.location.href="news.html";
+}
