fb6b73f36bbb1d9a7a912ce4bfafe1f19523e2ab
Scuit <1581703889@qq.com>
2022-06-01 00:35:34 +0900

    更新了more/contact/biography/index的响应式页面

--- work.js
+++ work.js
@@ -17,3 +17,22 @@ function live61(){
 function backtonews(){
     window.location.href="news.html";
 }
+
+//回到顶部
+window.addEventListener("scroll",handle)
+            function handle(){
+                var oTop = document.body.scrollTop||document.documentElement.scrollTop
+                if(oTop>=200){
+                    gotop.style.display = "block";
+                }else{
+                    gotop.style.display = "none";
+                }
+
+                gotop.onclick = function(){
+                    window.scrollTo({
+                        top:0,
+                        behavior:"smooth"
+                    })
+                }
+            }
+    
